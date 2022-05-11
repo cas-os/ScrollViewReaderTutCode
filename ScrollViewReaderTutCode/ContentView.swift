0540088652a623310c5708b9559a19c05445c810
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-05-12 00:15:37 +0200

    /.1

--- ScrollViewReaderTutCode/ContentView.swift
+++ ScrollViewReaderTutCode/ContentView.swift
@@ -8,9 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var textFiledText: String = ""
+    @State var scrollToIndex: Int = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        
+        VStack {
+            TextField("Enter # here...", text: $textFiledText)
+                .frame(height: 55)
+                .border(Color.gray)
+                .padding(.horizontal)
+                .keyboardType(.numberPad)
+            
+            Button("Go to pad ## ...") {
+                withAnimation(.spring()) {
+                    //proxy.scrollTo(30, anchor: .center)
+                    if let index = Int(textFiledText) {
+                        scrollToIndex = index
+                    }
+                }
+                
+            }
+            
+            ScrollView {
+                ScrollViewReader { proxy in
+                    
+                    
+                    
+                    ForEach(0..<50) { index in
+                        Text("Item # : \(index)")
+                            .font(.headline)
+                            .frame(height: 200)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.orange)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            .padding()
+                            .id(index)
+                    }
+                    .onChange(of: scrollToIndex, perform: { value in
+                        proxy.scrollTo(value, anchor: nil)
+                        
+                    })
+                }
+                
+            }
+        }
     }
 }
 
