cc43ddcedce6e402fd689fdd68ec8d79896ded80
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-05-11 21:50:12 +0200

    Initial Commit

---
+++ ScrollViewReaderTutCode/ScrollViewReaderTutCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewReaderTutCodeApp.swift
+//  ScrollViewReaderTutCode
+//
+//  Created by HAL-9001 on 11/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewReaderTutCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
